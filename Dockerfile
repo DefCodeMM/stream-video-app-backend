1f031d2576af3af0a50e068cc95448852294fbe5
jmacura <jeremiaszmacura99@gmail.com>
2022-02-21 18:42:34 +0100

    backend-04 - Create database connection. # adjusted docker-compose, mongodb as container.

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,13 @@
-FROM node:17 As development
+FROM node:17
 
 WORKDIR /workspace/server
 
 COPY package*.json ./
 
-RUN npm install --only=development
+RUN npm install
 
 COPY . .
+
+EXPOSE 3000
 
 RUN npm run build
-
-
-FROM node:17 as production
-
-ARG NODE_ENV=production
-ENV NODE_ENV=${NODE_ENV}
-
-WORKDIR /workspace/server
-
-COPY package*.json ./
-
-RUN npm install --only=production
-
-COPY . .
-
-COPY --from=development /workspace/server/dist ./dist
-
-CMD ["node", "dist/main"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,18 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './module/user.module';
 
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.dvdwf.mongodb.net/${process.env.MONGO_TABLE_NAME}?retryWrites=true&w=majority`
+      `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@mongodb:27017/testowa`,
     ),
     UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule { }
